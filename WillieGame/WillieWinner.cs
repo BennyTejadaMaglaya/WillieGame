7326c2184a2faae6ad93717b39df5a7c5eb55891
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-18 06:42:56 -0400

    Exercise 2 - Part C

--- WillieGame/WillieWinner.cs
+++ WillieGame/WillieWinner.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace WillieGame
 {
+    /// <summary>
+    /// Static class to compare two WilieHand Objects and determine the winner
+    /// </summary>
     public static class WillieWinner
     {
+        /// <summary>
+        /// Pass two WillieHand objects
+        /// Return value 1 means the first hand won
+        /// Return value -1 means the second hand won
+        /// Return value 0 means a tie
+        /// </summary>
+        /// <param name="HandOne">First Hand</param>
+        /// <param name="HandTwo">Second Hand</param>
+        /// <returns></returns>
+        public static int Win(WillieHand HandOne, WillieHand HandTwo)
+        {
+            if (HandOne.Willie > HandTwo.Willie)
+            {
+                return 1;
+            }
+            else if (HandOne.Willie < HandTwo.Willie)
+            {
+                return -1;
+            }
+            else //Willie values are tied.
+            {
+                if (HandOne.Wonka > HandTwo.Wonka)
+                {
+                    return 1;
+                }
+                else if (HandOne.Wonka < HandTwo.Wonka)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
     }
 }
---
+++ WillieGameTests/WillieWinnerTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WillieGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillieGame.Tests
+{
+    [TestClass()]
+    public class WillieWinnerTests
+    {
+        #region Exercise 2 - Part C
+
+        /// <summary>
+        /// Ex2 UseCase C1 Ace,King .vs. Jack,7 - Winner -1
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC1()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(1, 13);
+            WillieHand HandTwo = new WillieHand(11, 7);
+            int expected = -1;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Ex2 UseCase C2 Ace,King .vs. 6,4 - Winner 1
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC2()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(1, 13);
+            WillieHand HandTwo = new WillieHand(6, 4);
+            int expected = 1;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Ex2 UseCase C3 Ace,King .vs. Queen,Ace - Winner 1
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC3()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(1, 13);
+            WillieHand HandTwo = new WillieHand(12, 1);
+            int expected = 1;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Ex2 UseCase C4 6,5 .vs. Ace,King - Winner -1
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC4()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(6, 5);
+            WillieHand HandTwo = new WillieHand(1, 13);
+            int expected = -1;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Ex2 UseCase C5 Jack,10 .vs. 10,Jack - Winner 0
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC5()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(11, 10);
+            WillieHand HandTwo = new WillieHand(10, 11);
+            int expected = 0;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// UseCase C6 King,10 .vs. Queen,King - Winner 0
+        /// Example of a tie with hands that are not just a mirror image
+        /// </summary>
+        [TestMethod()]
+        public void WinTestC6()
+        {
+            //Arrange
+            WillieHand HandOne = new WillieHand(13, 10);
+            WillieHand HandTwo = new WillieHand(12, 13);
+            int expected = 0;
+
+            //Act
+            int actual = WillieWinner.Win(HandOne, HandTwo);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
