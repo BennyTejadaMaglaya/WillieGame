343bb084da04d14b4fc5159704c4f47761409cb1
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-17 20:17:06 -0400

    Get the starter version of the class.

--- WillieGame/WillieHand.cs
+++ WillieGame/WillieHand.cs
@@ -2,6 +2,40 @@
 {
     public partial class WillieHand
     {
-
+        private int _CardOne;
+        private int _CardTwo;
+        private int _Willie;
+        public WillieHand(int CardOne, int CardTwo)
+        {
+            _CardOne = CardOne;
+            _CardTwo = CardTwo;
+        }
+        public int Willie
+        {
+            get
+            {
+                if (_CardOne > 10)
+                    _Willie = 10;
+                else _Willie = _CardOne;
+                if (_CardTwo > 10)
+                    _Willie = 10;
+                else _Willie = _CardTwo;
+                return _Willie;
+            }
+        }
+        public int CardOne
+        {
+            get
+            {
+                return _CardOne;
+            }
+        }
+        public int CardTwo
+        {
+            get
+            {
+                return _CardTwo;
+            }
+        }
     }
 }
