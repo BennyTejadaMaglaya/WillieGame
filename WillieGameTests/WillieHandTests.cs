5f4eec392379bdc0617ac933098dcbf290ac72a9
Teejay Madlangbayan <teejaymadlangbayan@gmail.com>
2025-03-17 20:29:02 -0400

    Right-click on the WillieHand method and select Create Unit Tests.

---
+++ WillieGameTests/WillieHandTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WillieGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WillieGame.Tests
+{
+    [TestClass()]
+    public class WillieHandTests
+    {
+        [TestMethod()]
+        public void WillieHandTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
